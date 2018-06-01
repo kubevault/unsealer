6e0e2c0dc4db363d8916c660da430a1ca17e15b5
nightfury1204 <nahid@appscode.com>
2018-06-01 16:03:19 +0600

    Revendor

--- pkg/worker/options.go
+++ pkg/worker/options.go
@@ -39,7 +39,7 @@ type WorkerOptions struct {
 	Vault      *vault.VaultOptions
 	Google     *google.Options
 	Aws        *aws.Options
-	Azure  *azure.Options
+	Azure      *azure.Options
 	Kubernetes *kubernetes.Options
 }
 
